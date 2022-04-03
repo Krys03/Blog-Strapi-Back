a4d3e6d1c929a0855f75bd2cab7f0d14802ff027
Christophe Lopez <christophelopez@Christophes-MacBook-Pro.local>
2022-04-03 17:41:38 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e2683922294909aea569194ba192c1ef'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '42c85cd6-2f99-4333-a1bc-6673a405c35c'
+};
